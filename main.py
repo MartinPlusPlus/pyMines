ab1c814ac84060b8f04db79aef5a34fa6e6228ee
Martin Eldredge <martin.eldredge1@gmail.com>
2019-12-26 19:40:13 -0700

    we made a bomb

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
-import pygame, sys
+import pygame as pg
+import sys
 from pygame.locals import *
+from screens import screen
 
-pygame.init()
-window = pygame.display.set_mode((600, 800))
+WHITE = ((255, 255, 255))
+BLACK = ((0, 0, 0))
+RED = ((255, 0, 0))
+GREEN = ((0, 255, 0))
+BLUE = ((0, 0, 255))
 
-pygame.display.set_caption("pyMines")
+DARK_WHITE = ((150, 150, 150))
+
+pg.init()
+window = pg.display.set_mode((600, 800))
+
+#         self.currentRoom = self.roomList[request]
+pg.display.set_caption("pyMines")
 window.fill((200, 200, 200))
-pygame.display.update()
-
+pg.display.update()
 
 def titleScreen():
-    # TODO
+    pg.font.init()
 
 running = True
 
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for event in pg.event.get():
+        # Close the program if the x is pressed
+        if event.type == pg.QUIT:
+            pg.font.quit()
             running = False
 
-pygame.quit()
+        bomb = pg.draw.rect(window, DARK_WHITE, (0, 0, 30, 30), 0)
+        pg.display.flip()
+
+pg.font.quit()
+pg.quit()
 sys.exit()
